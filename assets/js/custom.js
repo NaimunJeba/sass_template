5959e29a6170a499766dfbbfab6d061daa138eec
Naimun Jeba <naimun.jeba@sjinnovation.com>
2021-04-01 10:29:35 +0600

    first commit

---
+++ assets/js/custom.js
@@ -0,0 +1,12 @@
+
+$(document).ready(function () {
+    // $(".main-slider").owlCarousel();
+    $('.main-slider').owlCarousel({
+        loop: true,
+        items: 1,
+        dots: false,
+        nav: true
+
+    })
+
+});
